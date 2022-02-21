1d96502cfee7d3241cab4b942c565c7a6de525ff
Himani <himani.asrani@gslab.com>
2022-02-21 12:29:01 +0530

    Adding /auth endpoint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Authorize from './components/Authorize/Authorize';
 
 function App() {
   return (
@@ -8,6 +10,14 @@ function App() {
         <p>
           Chameleon Idp Demo App.
         </p>
+        <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<p>Home</p>}>
+          </Route>
+          <Route path="/authorize" element={<Authorize/>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/components/Authorize/Authorize.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export default function Authorize() {
+   const code = 'kk3cex8xoCxm_rPl1_RHqEXgG1E6B9kDDHWEYZBHnps';
+   const [redirect_uri, setRedirectUri] = useState('');  
+   React.useEffect(() => {
+    const redirect_uri = new URLSearchParams(
+        window.location.search.split("").slice(1).join("")
+    ).get("redirect_uri");
+
+    if(redirect_uri){
+        setRedirectUri(redirect_uri);
+    }
+    }, []);
+
+    const generateAuthCode = () => {
+        window.location.href = redirect_uri + "/#code="+code;
+    }
+
+  return <>
+    <label>Username: </label>
+    <label>Password: </label>
+    <button onClick={generateAuthCode}>Submit</button>
+  </>
+}
