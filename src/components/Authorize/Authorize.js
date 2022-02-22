37c71c7c712fb190cea57d8cabc10f1df7e65924
Himani <himani.asrani@gslab.com>
2022-02-22 10:45:27 +0530

    state changes

--- src/components/Authorize/Authorize.js
+++ src/components/Authorize/Authorize.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 export default function Authorize() {
    const code = 'kk3cex8xoCxm_rPl1_RHqEXgG1E6B9kDDHWEYZBHnps';
    const [redirect_uri, setRedirectUri] = useState('');  
+   const [state, setState] = useState('');  
+   
    React.useEffect(() => {
     const redirect_uri = new URLSearchParams(
         window.location.search.split("").slice(1).join("")
@@ -11,10 +13,21 @@ export default function Authorize() {
     if(redirect_uri){
         setRedirectUri(redirect_uri);
     }
+
+    const state = new URLSearchParams(
+        window.location.search.split("").slice(1).join("")
+    ).get("state");
+
+    if(state){
+        setState(state);
+    }
+
     }, []);
 
+
+
     const generateAuthCode = () => {
-        window.location.href = redirect_uri + "?code="+code+"&state=hgwvmqvdwqkbd";
+        window.location.href = redirect_uri + "?code="+code+"&state="+state;
     }
 
   return <>
